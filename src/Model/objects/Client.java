251c7c2a84886af23d9564981d6d2676204618f7
Alisa <sheridaniam@users.noreply.github.com>
2021-01-09 14:01:00 +0200

    Create Client.java

---
+++ src/Model/objects/Client.java
@@ -0,0 +1,27 @@
+package Model.objects;
+
+public class Client {
+    String firstname;
+    String lastname;
+
+    public Client(String firstName, String lastname) {
+        this.firstname = firstName;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
