3f5f4d91d2297ee9c408bc7a3a1f9e860df4734f
Alisa <sheridaniam@users.noreply.github.com>
2021-01-09 13:58:55 +0200

    Create LabelProcess.java

---
+++ src/View/labels/LabelProcess.java
@@ -0,0 +1,13 @@
+package View.labels;
+
+import javafx.scene.control.Label;
+
+
+public class LabelProcess {
+    
+    public static String inAProcess = "In a process...";
+    public static String ready = "Success!";
+    public static String awaiting = "Awaiting for the choose...";
+
+    public static Label process = new Label(awaiting);
+}
