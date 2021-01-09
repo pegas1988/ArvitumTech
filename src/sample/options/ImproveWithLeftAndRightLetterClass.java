52c6890a6a9dcaa8546624daf35c2cacde8aefc5
Alisa <sheridaniam@users.noreply.github.com>
2021-01-09 13:59:47 +0200

    Create ImproveWithLeftAndRightLetterClass.java

---
+++ src/sample/options/ImproveWithLeftAndRightLetterClass.java
@@ -0,0 +1,93 @@
+package sample.options;
+
+import Controller.DBController;
+import Controller.SearchScanClass;
+import ExceptionAr.ExceptionWindow;
+import Model.objects.Client;
+import View.tables.TableClients;
+import sample.Main;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLException;
+
+public class ImproveWithLeftAndRightLetterClass {
+    public static String scans [] = null;
+    public String arrayForLeftScans [] = null;
+    public String arrayForRightScans [] = null;
+    static File leftScans = null;
+    static File rightScans = null;
+    static String strLineForLeft;
+    static String strLineForRight;
+    static String tempNameOfScan;
+    static String result;
+
+    public static String improveMethod(String array [], File directory, String podopitniy) {
+        leftScans = new File(directory + "\\" + "_left.log");
+        rightScans = new File(directory + "\\" + "_right.log");
+
+        FileInputStream fstreamForLeft = null;
+        try {
+            fstreamForLeft = new FileInputStream(leftScans);
+        } catch (FileNotFoundException e) {
+            ExceptionWindow.problem.setText("log file with left scans were not found");
+            ExceptionWindow.window("Error!");
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader brForLeft = new BufferedReader(new InputStreamReader(fstreamForLeft));
+            while ((strLineForLeft = brForLeft.readLine()) != null) {
+                tempNameOfScan = strLineForLeft.substring(36, strLineForLeft.length() - 5);
+                if (tempNameOfScan.equals(podopitniy)) {
+                    return result = tempNameOfScan.substring(0, tempNameOfScan.length() - 4) + "L.DAT";
+                }
+            }
+
+        } catch (IOException | NullPointerException | StringIndexOutOfBoundsException e) {
+            //e.printStackTrace();
+        }
+
+        try {
+            fstreamForLeft.close();
+        } catch (IOException | NullPointerException e) {
+        }
+
+        FileInputStream fstreamForRight = null;
+        try {
+            fstreamForRight = new FileInputStream(rightScans);
+        } catch (FileNotFoundException e) {
+            ExceptionWindow.problem.setText("log file with left scans were not found");
+            ExceptionWindow.window("Error!");
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader brForRight = new BufferedReader(new InputStreamReader(fstreamForRight));
+            while ((strLineForRight = brForRight.readLine()) != null) {
+                tempNameOfScan = strLineForRight.substring(36, strLineForRight.length() - 5);
+                if (tempNameOfScan.equals(podopitniy)) {
+                    return result = tempNameOfScan.substring(0, tempNameOfScan.length() - 4) + "R.DAT";
+                }
+                else
+                    result = podopitniy;
+            }
+
+        } catch (IOException | NullPointerException | StringIndexOutOfBoundsException e) {
+            //e.printStackTrace();
+        }
+
+        try {
+            fstreamForRight.close();
+        } catch (IOException | NullPointerException e) {
+        }
+        return result;
+        }
+
+        public static void modifyScansWithLAndR (String originScan, String modifyed) throws SQLException, IOException {
+            File originFile = new File(DBController.getFromDBScanFolder() + "\\" + Main.easyDateGetting + "\\" + originScan);
+            File pathFile = new File(DBController.getFromDBScanFolder() + "\\" + Main.easyDateGetting + "\\" + modifyed);
+            Files.copy(originFile.toPath(), pathFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+
