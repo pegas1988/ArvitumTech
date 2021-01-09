06f7fac296473ccaa104076ec40924f0dc238d3e
Alisa <sheridaniam@users.noreply.github.com>
2021-01-09 13:59:03 +0200

    Create LabelFIO.java

---
+++ src/View/labels/LabelFIO.java
@@ -0,0 +1,12 @@
+package View.labels;
+
+
+import javafx.scene.control.Label;
+
+public class LabelFIO {
+    public static String clientFio = "fio";
+    public void setFio(String fio) {
+        this.clientFio = fio;
+    }
+    public static Label fio = new Label("Wait");
+}
