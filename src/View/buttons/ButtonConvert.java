7b397b76eb283cb813b26351fd9f33e51f0764ee
Alisa <sheridaniam@users.noreply.github.com>
2021-01-09 13:59:19 +0200

    Create ButtonConvert.java

---
+++ src/View/buttons/ButtonConvert.java
@@ -0,0 +1,14 @@
+package View.buttons;
+
+import Controller.ConverterClass;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import sample.Main;
+
+public class ButtonConvert{
+    public static Button buttonConvert = new Button("Convert");
+
+
+
+}
